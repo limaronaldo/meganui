75ab6bd79d9af4cd8143126134a99f87b0469284
Paulo Melo Jr <paulomelojunior@icloud.com>
2018-11-21 19:24:49 -0200

    More colors for buttons

--- src/lib/Button/index.js
+++ src/lib/Button/index.js
@@ -6,7 +6,6 @@ export class Button extends Component {
     render() {
         return (
             <div>
-
                 <button className="button">{this.props.children}</button>
                 <button className="button primary">{this.props.children}</button>
                 <button className="button primary-variant">{this.props.children}</button>
@@ -18,14 +17,30 @@ export class Button extends Component {
                 <button className="button big secondary">{this.props.children}</button>
                 <br></br>
                 <button className="button bigger outlined">{this.props.children}</button>
-                <button className="button bigger primary outlined">{this.props.children}</button>
-                <button className="button bigger primary-variant outlined">{this.props.children}</button>
-                <button className="button bigger secondary outlined">{this.props.children}</button>
+                <button className="button bigger outlined primary">{this.props.children}</button>
+                <button className="button bigger outlined primary-variant">{this.props.children}</button>
+                <button className="button bigger outlined secondary">{this.props.children}</button>
                 <br></br>
                 <button className="button biggest" disabled>{this.props.children}</button>
                 <button className="button biggest primary" disabled>{this.props.children}</button>
                 <button className="button biggest primary-variant" disabled>{this.props.children}</button>
                 <button className="button biggest secondary" disabled>{this.props.children}</button>
+                <br></br>
+                <button className="button alert">{this.props.children}</button>
+                <button className="button warn">{this.props.children}</button>
+                <button className="button success">{this.props.children}</button>
+                <br></br>
+                <button className="button big alert">{this.props.children}</button>
+                <button className="button big warn">{this.props.children}</button>
+                <button className="button big success">{this.props.children}</button>
+                <br></br>
+                <button className="button bigger outlined alert">{this.props.children}</button>
+                <button className="button bigger outlined warn">{this.props.children}</button>
+                <button className="button bigger outlined success">{this.props.children}</button>
+                <br></br>
+                <button disabled className="button biggest alert">{this.props.children}</button>
+                <button disabled className="button biggest warn">{this.props.children}</button>
+                <button disabled className="button biggest success">{this.props.children}</button>
             </div>
         );
     }
