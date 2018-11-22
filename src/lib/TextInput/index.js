7dc14942d617e73ff3b08503703f9f7d595a9323
Paulo Melo Jr <paulomelojunior@icloud.com>
2018-11-22 15:29:25 -0200

    Text input component created

---
+++ src/lib/TextInput/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './index.scss';
+
+export class TextInput extends Component {
+
+    render() {
+        return (
+            <input type="text"></input>
+        );
+    }
+
+}
