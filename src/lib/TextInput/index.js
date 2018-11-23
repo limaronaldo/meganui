4c6d430b8bb2d3636cb96ed84eba69cbff076a0a
Paulo Melo Jr <paulomelojunior@icloud.com>
2018-11-23 11:47:55 -0200

    Adding sizes

--- src/lib/TextInput/index.js
+++ src/lib/TextInput/index.js
@@ -5,7 +5,12 @@ export class TextInput extends Component {
 
     render() {
         return (
-            <input type="text"></input>
+            <div>
+                <input className="form-control" type="text"></input>
+                <input className="form-control big" type="text"></input>
+                <input className="form-control bigger" type="text"></input>
+                <input className="form-control biggest" type="text"></input>
+            </div>
         );
     }
 
