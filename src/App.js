cd64e9a213424e2cc15d3cc15c1f32b95a70bfae
Jimmy Andrade <jimmy.andrade@vulpi.com.br>
2018-11-28 11:47:26 -0200

    Added props to Button

--- src/App.js
+++ src/App.js
@@ -5,15 +5,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Heading level="1">h1. Megan heading</Heading>
-        <Heading level="2">h2. Megan heading</Heading>
-        <Heading level="3">h3. Megan heading</Heading>
-        <Heading level="4">h4. Megan heading</Heading>
-        <Heading level="5">h5. Megan heading</Heading>
-        <Heading level="6">h6. Megan heading</Heading>
-        <Button>
-          First Button
-        </Button>
+        <p><Button>First Button</Button></p>
+        <p><Button block>First Button</Button></p>
+        <p>
+          
+        </p>
         <TextInput/>        
         <Text/>
       </div>
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { Heading } from '../lib';
+import { Button, Heading } from '../lib';
 
 storiesOf('Heading', module)
   .add('Level 1', () => (<Heading level="1">This is a level one heading</Heading>))
@@ -10,4 +10,28 @@ storiesOf('Heading', module)
   .add('Level 4', () => (<Heading level="4">This is a level four heading</Heading>))
   .add('Level 5', () => (<Heading level="5">This is a level five heading</Heading>))
   .add('Level 6', () => (<Heading level="6">This is a level six heading</Heading>))
+;
+
+storiesOf('Button', module)
+  .add('Colors', () => (
+      <div>
+        <Button>Basic Bitch</Button>
+        <Button color="primary">Primary</Button>
+        <Button color="primary-variant">Primary Variant</Button>
+        <Button color="secondary">Secondary</Button>
+        <Button color="danger">Danger</Button>
+        <Button color="warning">Warning</Button>
+        <Button color="success">Success</Button>
+      </div>
+    )
+  )
+  .add('Sizes', () => (
+      <div>
+        <Button>Normal</Button>
+        <Button size="big">Big</Button>
+        <Button size="bigger">Bigger</Button>
+        <Button size="biggest">Biggest</Button>
+      </div>
+    )
+  )
 ;
